c4135d6aaed35f2839b7ddfab6eb09d5677630e6
Goblinekkk <matejzeleny13@gmail.com>
2026-05-26 20:13:56 +0200

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyCKHwLpj76OU8013qSEsUnC01HishLTmo4",
+
+  authDomain: "brawlstarschampionship.firebaseapp.com",
+
+  projectId: "brawlstarschampionship",
+
+  storageBucket: "brawlstarschampionship.firebasestorage.app",
+
+  messagingSenderId: "700262812743",
+
+  appId: "1:700262812743:web:4cf2fa1680b68e6be67562",
+
+  measurementId: "G-309MYX7F2H"
+
+};
+
+export const app =
+initializeApp(firebaseConfig);
